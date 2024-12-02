ac17b2c5b89e3657fad035b1a3f1b202211252a6
Muhammad Rohan <164128040+mianrohan623@users.noreply.github.com>
2024-12-03 00:24:37 +0500

    user dashboard main tab added

--- src/components/dashboard/user/SidebarLayout.js
+++ src/components/dashboard/user/SidebarLayout.js
@@ -1,10 +1,50 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+// Import Image component from next
+import Image from 'next/image';
+
+// This is logo image variable
+const Logo = "/images/logo.png";
 
 export default function SidebarLayout() {
     return (
         <>
-            <h1 className="text-white">
-                sidebar
-            </h1>
+            <div className="pt-0 md:pt-7 p-3 md:p-4 h-full">
+                <div className="flex items-center justify-center mb-4 md:mb-5">
+                    <div className="relative h-46px w-200px ">
+                        <Image
+                            src={Logo}
+                            alt="Logo"
+                            layout="fill"  // This makes the image fill the parent container
+                            // objectFit="contain"  // Ensures the image maintains its aspect ratio
+                            className="object-contain"  // Optional: any other styling
+                        />
+                    </div>
+                </div>
+
+                <Tabs defaultValue="channels" className="w-full">
+                    <TabsList className="mb-4 md:mb-5 bg-transparent flex gap-x-3 px-2 py-3 md:py-4 h-auto border-b border-border-color rounded-none">
+                        <TabsTrigger
+                            value="channels"
+                            className="capitalize text-base md:text-lg flex-grow h-46px flex items-center justify-center rounded-md font-medium data-[state=active]:bg-theme-gradient data-[state=active]:text-white data-[state=inactive]:bg-transparent border border-primary-color data-[state=inactive]:text-white">
+                            channels
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="friends"
+                            className="capitalize text-base md:text-lg flex-grow h-46px flex items-center justify-center rounded-md font-medium data-[state=active]:bg-theme-gradient data-[state=active]:text-white data-[state=inactive]:bg-transparent border border-primary-color data-[state=inactive]:text-white">
+                           friends
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="channels" className="text-white">
+                        Channels tab content
+                    </TabsContent>
+                    <TabsContent value="friends" className="text-white">
+                        friends tab content
+                    </TabsContent>
+                </Tabs>
+
+            </div>
+
         </>
     )
 }
---
+++ src/components/dashboard/user/sidbar/SidebarMainTabs.js
@@ -0,0 +1,7 @@
+export default function SidebarMainTabs() {
+    return (
+        <div>
+            this is main tab component
+        </div>
+    );
+}
